72d3940828503d249aac3949ef25fedb5d1b3743
bellemere <levhkim@gmail.com>
2022-04-01 20:50:35 -0400

    first commit

---
+++ components/Box.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Box() {
+    return <mesh>
+        <boxBufferGeometry attach="geometry" />
+        <meshLambertMaterial attach="material" color="blue" />
+    </mesh>;
+}
