aaff0bfdbef2cace9df27ab6452d6b0824901d83
bellemere <levhkim@gmail.com>
2022-04-02 17:44:50 -0400

    remove imports

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import GameWorld from "../components/GameWorld";
-import { Abi } from 'starknet'
 import {
   StarknetProvider,
 } from '@starknet-react/core'
