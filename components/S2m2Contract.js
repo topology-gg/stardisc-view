50990e066b3e2e9e0ec942c1be5fae7d11eec9b1
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-07-28 14:26:39 +0800

    update contract address

--- components/S2m2Contract.js
+++ components/S2m2Contract.js
@@ -3,7 +3,7 @@ import {
 } from '@starknet-react/core'
 
 import S2m2Abi from '../abi/s2m2_abi.json'
-const S2M2_ADDR = '0x02369dbd0ec5e3e152aef28d10042abdf7a22a316c667e2a880bd4c0978e448b'
+const S2M2_ADDR = '0x039d38747fb62279cb5266261b01dce9bf369b53fe422e89fcb8153891e301f9'
 
 export function useS2m2Contract () {
     return useContract ({ abi: S2m2Abi, address: S2M2_ADDR })
