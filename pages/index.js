2f4c603a549d6f91d415a7019b55f1122b110055
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-07-30 23:26:06 +0800

    add link to deployed contract

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,12 @@ function Home() {
                     <p style={{marginBottom:'0'}}>Earn your ticket to Isaac alpha.</p>
                     <p style={{marginTop:'0',marginBottom:'0'}}>Control: mouse down and drag to select path; ESC to reset</p>
                     <p style={{marginTop:'0',marginBottom:'0'}}>
-                        <a href="https://github.com/topology-gg/s2m2/tree/main/contracts" style={{color:'#DDDDDD'}} target="_blank" rel="noopener noreferrer">
-                            Link to contracts
+                        <a href="https://github.com/topology-gg/s2m2/tree/main/contracts" style={{color:'#DDDDDD',marginRight:'0.5em'}} target="_blank" rel="noopener noreferrer">
+                            Link to the source
+                        </a>
+                        /
+                        <a href="https://goerli.voyager.online/contract/0x039d38747fb62279cb5266261b01dce9bf369b53fe422e89fcb8153891e301f9" style={{color:'#DDDDDD',marginLeft:'0.5em'}} target="_blank" rel="noopener noreferrer">
+                            Link to the deployed contract
                         </a>
                     </p>
 
