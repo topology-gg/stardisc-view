57350f59d4e9af3ad1b14b135d36764828c98c6f
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-07-30 20:42:08 +0800

    change site title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import NextHead from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+        <NextHead>
+            <title>S2M2</title>
+        </NextHead>
+        <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
